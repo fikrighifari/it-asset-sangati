76624d70a2670b27977eca730dd417a82208dd16
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-08-31 17:02:14 +0700

    update: barang model, view, route

--- app/Controllers/Barang.php
+++ app/Controllers/Barang.php
@@ -1,21 +1,17 @@
 <?php
-
 namespace App\Controllers;
-
-use App\Models\Barang_model;
 use CodeIgniter\Controller;
+use App\Models\Barang_model;
 
 class Barang extends Controller
 {
-
-    public function index($request)
+    public function index()
     {
-        $model = new Barang_model();
-        $data['title'] = 'Data Barang';
+        $model = new Barang_model;
+        $data['title']     = 'Data Barang';
         $data['getBarang'] = $model->getBarang();
-        echo view('header_view', $data);
-        echo view('barang_view', $data);
-        echo view('footer_view', $data);
-
+        echo view('barang/header_view', $data);
+        echo view('barang/barang_view', $data);
+        echo view('barang/footer_view', $data);
     }
 }
---
+++ app/Views/barang/header_view.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title><?= $title; ?></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav class="nav justify-content-center">
+        <!-- <div class="container">
+        <nav class="navbar navbar-expand navbar-light bg-light">
+            <div class="nav navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Home</a>
+            </div>
+        </nav>
+    </div> -->
+        <div class="container">
+            <a class="navbar-brand" href="<?= base_url(); ?>">Data Barang IT Sangati</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
+        </div>
+
+    </nav>
+</body>
+
+</html>
