e78087fa85b106bb1a222fc549a70cb35bc97d36
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-09-01 17:30:55 +0700

    add: tambah view

--- app/Controllers/Barang.php
+++ app/Controllers/Barang.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Controllers;
+
 use CodeIgniter\Controller;
 use App\Models\Barang_model;
 
@@ -10,6 +12,14 @@ class Barang extends Controller
         $model = new Barang_model;
         $data['title']     = 'Data Barang';
         $data['getBarang'] = $model->getBarang();
+        echo view('barang/header_view', $data);
+        echo view('barang/barang_view', $data);
+        echo view('barang/footer_view', $data);
+    }
+
+    public function tambah()
+    {
+        $data['title'] = 'Tambah Data Barang';
         echo view('barang/header_view', $data);
         echo view('barang/barang_view', $data);
         echo view('barang/footer_view', $data);
---
+++ app/Views/barang/tambah_view.php
@@ -0,0 +1,42 @@
+<div class="container p-5">
+    <a href="<?= base_url('barang'); ?>" class="btn btn-secondary mb-2"> Kembali </a>
+    <div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="container p-5">
+    <a href="<?= base_url('barang');?>" class="btn btn-secondary mb-2">Kembali</a>
+    <div class="card">
+        <div class="card-header bg-info text-white">
+            <h4 class="card-title">Tambah Data Barang</h4>
+        </div>
+        <div class="card-body">
+            <form method="post" action="<?= base_url('barang/add');?>">
+                <div class="form-group">
+                    <label for="">Nama Barang</label>
+                    <input type="text" name="nama" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Qty</label>
+                    <input type="number" name="qty" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Harga Beli</label>
+                    <input type="number" name="beli" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Harga Jual</label>
+                    <input type="number" name="jual" class="form-control" required>
+                </div>
+                <button class="btn btn-success">Tambah Data</button>
+            </form>
+            
+        </div>
+    </div>
+</div>
