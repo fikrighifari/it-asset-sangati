5a61257e1545260c22d885ed786912045975da51
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-09-05 14:40:48 +0700

    update: delete models

--- app/Models/Barang_model.php
+++ app/Models/Barang_model.php
@@ -22,11 +22,12 @@ class Barang_model extends Model
         return $builder->insert($data);
     }
 
-    // public function deleteBarang($id)
-    // {
-    //     $builder = $this->db->table($this->table);
-    //     return $builder->delete(['id_barang', $id]);
-    // }
+    public function deleteBarang($id)
+    {
+        $builder = $this->db->table($this->table);
+        $builder->where('id_barang', $id);
+        $builder->delete();
+    }
 
     public function editBarang($data, $id)
     {
@@ -35,10 +36,10 @@ class Barang_model extends Model
         return $builder->update($data);
     }
 
-    public function deleteBarang($id)
-    {
-        $stQuery = "DELETE FROM ".$this->table." ";
-        $stQuery .= "WHERE id_barang = ".$this->db->escapeString($id);
-        $this->db->query($stQuery);
-    }
+    // public function deleteBarang($id)
+    // {
+    //     $stQuery = "DELETE FROM ".$this->table." ";
+    //     $stQuery .= "WHERE id_barang = ".$this->db->escapeString($id);
+    //     $this->db->query($stQuery);
+    // }
 }
